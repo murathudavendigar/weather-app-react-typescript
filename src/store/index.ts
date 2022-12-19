85be58f308aec894bfd37314165440d0ab5718ea
Captain Price <murathoncu@gmail.com>
2022-12-20 02:50:35 +0300

    project started

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import weatherReducer from "./reducers/weatherReducer";
+import alertReducer from "./reducers/alertReducer";
+
+const rootReducer = combineReducers({
+  weather: weatherReducer,
+  alert: alertReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default store;
