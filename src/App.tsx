378b90109cbbfa2c8b6df94fc258b59008b6210b
Captain Price <murathoncu@gmail.com>
2023-01-05 19:55:11 +0300

    project start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div className="flex justify-center items-center bg-gradient-to-br from-sky-500  to-gray-300 h-[100vh] w-full">
-      Hello Weather App
+      <section className="bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded">
+        Hello Weather App
+      </section>
     </div>
   );
 };
